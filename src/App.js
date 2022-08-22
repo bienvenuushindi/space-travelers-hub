89cbc7a4dce4b89d6a89ce1dfa8204990f3b781f
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-08-22 19:08:04 +0500

    Fixed Linter errors and deleted unused files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <div className="App" />
   );
 }
 
