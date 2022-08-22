33c11c813fba9585ce9019fc524921a33d0e5b60
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-08-22 18:46:27 +0500

    Updated linters version

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
+import planet from '../images/planet.png';
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div className='mainHeader'>
+      <header>
+        <div className='logo'>
+
+        </div>
+        <h1>Space Traveler's Hub</h1>
+        <nav>
+          
+        </nav>
+      </header>
+
+    </div>
   )
 }
 
