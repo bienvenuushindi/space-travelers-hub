44109e53672926f194a5eb3acd61fa774baa1c90
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-08-22 19:24:18 +0500

    Updated code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Profile from './Components/Profile/Profile';
 import Nav from './Components/Nav';
 
 function App() {
-<<<<<<< HEAD
   <div className="App">
     <Nav />
     <Routes>
@@ -16,12 +15,6 @@ function App() {
       <Route path="/profile" element={<Profile />} />
     </Routes>
   </div>
-=======
-  return (
-    <div className="App">
-    </div>
-  );
->>>>>>> 35b4adbf89eb807c22decd67eec51e3b3cb3ed06
 }
 
 export default App;
