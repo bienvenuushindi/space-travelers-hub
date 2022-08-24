83b0ee71935dfe20e40d15f597ca568bd5a7c1c0
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-08-24 11:46:15 +0500

    Handled Profile for Missions, only styling left

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Mission from './Components/Mission/Mission';
+import Profile from './Components/Profile/Profile';
+import Rocket from './Components/Rockets/Rocket';
 import Nav from './Components/Nav';
 
 function App() {
@@ -9,7 +11,9 @@ function App() {
     <div className="App">
       <Nav />
       <Routes>
+        <Route exact path="/" element={<Rocket />} />
         <Route path="/missions" element={<Mission />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
   );
