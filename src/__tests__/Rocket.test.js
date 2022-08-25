6e1ec912e0721e86f89463cb1538bb1b78979203
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-08-25 18:30:50 +0500

    Added Rocket test

--- src/__tests__/Rocket.test.js
+++ src/__tests__/Rocket.test.js
@@ -4,13 +4,13 @@ import '@testing-library/jest-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '../Components/Redux/spaceStore';
-import Missions from '../Components/Mission/Mission';
+import Rockets from '../Components/Rockets/Rocket';
 
 it('Test Mission component', () => {
   const tree = render(
     <Provider store={store}>
       <Router>
-        <Missions />
+        <Rockets />
       </Router>
     </Provider>,
   );
